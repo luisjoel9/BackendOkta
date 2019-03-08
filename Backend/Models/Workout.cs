71dc6228ef7b163fa9a5fff7597bd68638e45beb
Joel <joel.luis.cr@gmail.com>
2019-03-08 18:53:44 -0400

    First commit

---
+++ Backend/Models/Workout.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models
+{
+    public class Workout
+    {
+        public int Id { get; set; }
+        [Required]
+        public DateTimeOffset Date { get; set; }
+        public int DistanceInMeters { get; set; }
+        [Required]
+        public long TimeInSeconds { get; set; }
+    }
+}
